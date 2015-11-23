9100132c3865bd8ef7285af3204df0dfbfed20bf
reunisoft <reunisoft@gmail.com>
2015-11-23 19:39:02 +0100

    Signed-off-by: reunisoft <reunisoft@gmail.com>

---
+++ app/src/main/java/com/reunisoft/intelsms/intelsms/MainActivity.java
@@ -0,0 +1,140 @@
+package com.reunisoft.intelsms.intelsms;
+
+import android.content.Context;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.content.res.Resources;
+import android.database.Cursor;
+import android.graphics.Color;
+import android.net.Uri;
+import android.net.wifi.WifiManager;
+import android.os.AsyncTask;
+import android.os.Bundle;
+import android.provider.Telephony;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.telephony.SmsManager;
+import android.telephony.SmsMessage;
+import android.text.format.Formatter;
+import android.util.Log;
+import android.view.Gravity;
+import android.view.View;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.Button;
+import android.widget.LinearLayout;
+import android.widget.ScrollView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import java.io.BufferedOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.util.Timer;
+import java.util.TimerTask;
+import java.util.UUID;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+    private static final String TAG = "intelsms";
+    Button buttonStart, buttonStop;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+            }
+        });
+
+        buttonStart = (Button) findViewById(R.id.btnStartServer);
+        buttonStop = (Button) findViewById(R.id.btnStopServer);
+
+        buttonStart.setOnClickListener(this);
+        buttonStop.setOnClickListener(this);
+        LinearLayout linearLayout = (LinearLayout) findViewById(R.id.linearLayout);
+        linearLayout.setBackgroundColor(Color.BLACK);
+
+        WifiManager wm = (WifiManager) getSystemService(WIFI_SERVICE);
+        String ip = Formatter.formatIpAddress(wm.getConnectionInfo().getIpAddress());
+        wm = null;
+        log("IP for server : " + ip);
+        //SmsManager smsManager = SmsManager.getDefault();
+        //smsManager.sendTextMessage("0763231047", "+33660003000", "sms message", null, null);
+    }
+
+    public void onClick(View src) {
+        switch (src.getId()) {
+            case R.id.btnStartServer:
+                Log.d(TAG, "onClick: starting service");
+                startService(new Intent(this, MyService.class));
+                break;
+            case R.id.btnStopServer:
+                Log.d(TAG, "onClick: stopping service");
+                stopService(new Intent(this, MyService.class));
+                break;
+        }
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    private int currentTextColor = Color.GREEN;
+    public void log(String str) {
+        final LinearLayout linearLayout = (LinearLayout) findViewById(R.id.linearLayout);
+
+        // Si plus de 1000 lignes dans le log à l'écran alors effacer la zone de log
+        if (linearLayout.getChildCount() > 1024) {
+            linearLayout.removeAllViews();
+        }
+
+        final TextView textView = new TextView(MainActivity.this);
+        textView.setTextColor(currentTextColor);
+        if (currentTextColor == Color.GREEN) currentTextColor = Color.LTGRAY; else currentTextColor = Color.GREEN;
+
+        textView.setTextSize(12);
+        textView.setText(str);
+        linearLayout.addView(textView);
+
+        ((ScrollView) findViewById(R.id.scrollView)).post(new Runnable() {
+            public void run() {
+                ((ScrollView) findViewById(R.id.scrollView)).fullScroll(View.FOCUS_DOWN);
+            }
+        });
+
+    }
+
+}
---
+++ app/src/main/java/com/reunisoft/intelsms/intelsms/MyService.java
@@ -0,0 +1,227 @@
+package com.reunisoft.intelsms.intelsms;
+
+/**
+ * Created by Idjed on 22/11/2015.
+ */
+import android.app.Service;
+import android.content.Intent;
+//import android.media.MediaPlayer;
+import android.database.Cursor;
+import android.graphics.Color;
+import android.net.Uri;
+import android.net.wifi.WifiManager;
+import android.os.IBinder;
+import android.text.format.Formatter;
+import android.util.Base64;
+import android.util.Log;
+import android.view.View;
+import android.widget.LinearLayout;
+import android.widget.ScrollView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.text.DateFormat;
+import java.text.FieldPosition;
+import java.text.ParseException;
+import java.text.ParsePosition;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Date;
+import java.util.UUID;
+
+public class MyService extends Service {
+    private static final String TAG = "intelsms-service";
+
+    @Override
+    public IBinder onBind(Intent intent) {
+        return null;
+    }
+
+    @Override
+    public void onCreate() {
+        Toast.makeText(this, "IntelSMS Service Created", Toast.LENGTH_LONG).show();
+        Log.d(TAG, "onCreate");
+    }
+
+    @Override
+    public void onDestroy() {
+        try{
+            Toast.makeText(this, "IntelSMS Service Stopped", Toast.LENGTH_LONG).show();
+        }catch (Exception ex){}
+        Log.d(TAG, "onDestroy");
+        enabled = false;
+        try{
+            if (socket !=null) socket.close();
+            Log.d(TAG,"socket closed");
+        }catch (IOException e){}
+        try{
+            if (serverSocket !=null) serverSocket.close();
+            Log.d(TAG, "server socket closed");
+        }catch (IOException e){}
+    }
+
+    @Override
+    public int onStartCommand(Intent intent, int flags, int startid) {
+        Log.d(TAG, "onStart");
+        enabled = true;
+        startServer();
+        return START_NOT_STICKY;
+    }
+
+    boolean enabled = true;
+    ServerSocket serverSocket = null;
+    Socket socket = null;
+    public void startServer() {
+        Log.d(TAG,"startServer");
+
+        UUID uuid = UUID.randomUUID();
+        final String sessionId = uuid.toString().replace("-", "");
+        Log.d(TAG, "Session ID = " + sessionId);
+
+        WifiManager wm = (WifiManager) getSystemService(WIFI_SERVICE);
+        String ip = Formatter.formatIpAddress(wm.getConnectionInfo().getIpAddress());
+        wm = null;
+        Log.d(TAG, "IP=" + ip);
+        if (ip.equals("0.0.0.0"))
+        {
+            Log.d(TAG, "Cannot start server because no WIFI IP address.");
+            return;
+        }
+
+        Thread t = new Thread(new Runnable() {
+            @Override
+            public void run() {
+                while(enabled==true)
+                {
+                    try {
+                        if (serverSocket == null) {
+                            serverSocket = new ServerSocket(7777);
+                        }
+
+                        serverSocket.setReuseAddress(true);
+
+                        if (socket == null)
+                            socket = serverSocket.accept();
+
+                        if (socket != null) {
+                            socket.close();
+                            socket = serverSocket.accept();
+                        }
+
+                        Log.d(TAG, "client connected");
+
+                        /*if (socket != null)
+                            if (socket.isClosed())
+                                socket = serverSocket.accept();*/
+
+                        assert socket != null;
+                        InputStream inputStream = socket.getInputStream();
+                        String strFromClient = "";
+                        int available = inputStream.available();
+                        Log.d(TAG, "available from client:" + available);
+                        for (int i=0;i<available;i++){
+                            int c = inputStream.read();
+                            strFromClient+=(char)c;
+                        }
+
+                        Log.d(TAG, "received from client:" + strFromClient);
+
+                        OutputStream outputStream = socket.getOutputStream();
+                        if (strFromClient.toLowerCase().trim().equals("hello from client")){
+                            String strToClient = "hello from server";
+                            outputStream.write(strToClient.getBytes());
+                            outputStream.flush();
+                            Log.d(TAG, "sent to client:" + strToClient);
+                        }
+                        else if (strFromClient.toLowerCase().trim().equals("get sms")){
+                            String sms = getSmsMessages();
+                            outputStream.write(sms.getBytes());
+                            outputStream.flush();
+                            Log.d(TAG, "bytes sent to client:" + sms.length());
+                        }
+
+                        outputStream.close();
+                        inputStream.close();
+
+                        //socket.close();
+                        //serverSocket.close();
+
+                        Log.d(TAG, "end of server processing");
+                    } catch (Exception e) {
+                        Log.d(TAG, "001:" + e.toString());
+                    }
+                }
+            }
+        });
+        t.start();
+    }
+
+    public String getSmsMessages(){
+        try {
+            // public static final String INBOX = "content://sms/inbox";
+            // public static final String SENT = "content://sms/sent";
+            // public static final String DRAFT = "content://sms/draft";
+            Cursor cursor = getContentResolver().query(Uri.parse("content://sms/inbox"), null, null, null, null);
+            String msgData = "", colName="", value="";
+            assert cursor != null;
+            String encoded = "", date="";
+            if (cursor.moveToFirst()) { // must check the result to prevent exception
+                do {
+                    for (int idx = 0; idx < cursor.getColumnCount(); idx++) {
+                        colName = cursor.getColumnName(idx);
+                        value = cursor.getString(idx);
+
+                        msgData += " " + colName + ":";
+
+                        try {
+                            if (!colName.contains("date")) {
+                                encoded = Base64.encodeToString(value.getBytes(), Base64.DEFAULT);
+                                msgData += encoded;
+                            }
+                            else if (colName.contains("date")){
+                                if (!value.contains("null")) {
+                                    date = convertMillisecondsToDate(value);
+                                    encoded = Base64.encodeToString(date.getBytes(), Base64.DEFAULT);
+                                    msgData += encoded;
+                                }
+                            }
+                        } catch (Exception ex){
+                            msgData += value;
+                        }
+                    }
+                    // use msgData
+                } while (cursor.moveToNext());
+            } else {
+                // empty box, no SMS
+            }
+            cursor.close();
+            return msgData;
+        }catch (Exception ex){
+            Log.d(TAG, ex.toString());
+        }
+        return "";
+    }
+
+    private String convertMillisecondsToDate(String value) {
+        String finalDateString = "";
+        try {
+            long milliSeconds = Long.parseLong(value);
+            DateFormat formatter = new SimpleDateFormat("dd/MM/yyyy HH:mm:ss.SSS");
+            Calendar calendar = Calendar.getInstance();
+            calendar.setTimeInMillis(milliSeconds);
+            finalDateString = formatter.format(calendar.getTime());
+            Log.d(TAG, "date parsed =" + finalDateString);
+        }
+        catch(Exception pe) {
+            Log.d(TAG, "date parsing exception:" + pe.toString());
+        }
+        return finalDateString;
+    }
+
+}
+
